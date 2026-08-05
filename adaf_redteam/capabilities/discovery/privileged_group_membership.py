2020c680580ab6af4d3dd83516f364fed01728db
rikterskale <31414158+rikterskale@users.noreply.github.com>
2026-08-05 15:06:04 -0400

    fix(discovery): use lab bind identity for privileged-group live path

--- adaf_redteam/capabilities/discovery/privileged_group_membership.py
+++ adaf_redteam/capabilities/discovery/privileged_group_membership.py
@@ -12,6 +12,7 @@ sorted list of member DNs plus counts split into users / computers / groups.
 
 from __future__ import annotations
 
+from ...lab_env import lab_bind_password, lab_bind_user, lab_dc
 from ..base import Capability, CapabilityResult
 
 
@@ -64,4 +65,13 @@ class PrivilegedGroupMembershipCapability(Capability):
 
     def _live_source(self):
         from ...directory.ldap_source import LdapDirectorySource
-        return LdapDirectorySource(self.domain, user=self.action.source_address)
+        bind_user = lab_bind_user() or self.action.source_address
+        server = lab_dc() or self.domain
+        password = lab_bind_password()
+        use_ccache = password is None
+        return LdapDirectorySource(
+            server,
+            user=bind_user,
+            use_kerberos_ccache=use_ccache,
+            password=password,
+        )
